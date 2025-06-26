39c351fca4605eb8056397952b55edf5e89a5017
pravin <thombalpravin5751@gmail.com>
2025-06-26 10:36:12 +0530

    connect db

---
+++ db.js
@@ -0,0 +1,19 @@
+let mysql=require("mysql2");
+
+require("dotenv").config();
+
+let conn=mysql.createconnect({
+    host:process.env.db_host,
+    password:process.env.db_password,
+    user:process.env.db_user,
+    database:process.env.db_database
+})
+
+conn.connect=(req,res)=>{
+    if(err){
+        console.log("database is not connect..");
+    }
+    else{
+        console.log("Database is Connected......");
+    }
+}
