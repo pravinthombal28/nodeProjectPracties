3d9b481aa469871c92af1ac425a853c17d6d8b76
BariKunal <kunalbari751@gmail.com>
2025-07-02 15:54:24 +0530

    Dependencies of project implemented

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+let path = require("path")
 let express=require("express");
 let db=require("../db.js");
 let bodyParser=require("body-parser");
@@ -6,22 +6,9 @@ require("dotenv").config();
 let app=express();
 
 app.set("view engine","ejs");
-app.use(express.static,("public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
 
 
 module.exports=app;
-=======
-let express = require("express")
-let bodyParser = require("body-parser")
-let msql = require("msql2")
-let router = require.Router()
-let app = express()
-
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.json)
-app.set("views engine","ejs")
-
-module.exports = app
->>>>>>> 9e65d505d3be7138499fc702a28ee5863f2168a1
